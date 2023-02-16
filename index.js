994ed31318001431e413c340ddaca9bcd952c8b8
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-02-16 09:08:32 +0200

    'main'

--- index.js
+++ index.js
@@ -11,8 +11,14 @@ let corsOption = {
 app.use(cors(corsOption));
 
 const userRouter = require('./routes/userRoute');
+const orderRouter = require('./routes/ordersRoute');
+const categoriesRouter = require('./routes/categoriesRoute');
+const productsRouter = require('./routes/productsRoute');
 
 app.use('/users', userRouter);
+app.use('/orders', orderRouter);
+app.use('/categories', categoriesRouter);
+app.use('/products', productsRouter);
 
 app.get('/', (req, res) =>{
     res.json({ message: "Welcome To The Store!!!"});
